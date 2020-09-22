a627ba551f5668348b8c92b6b1ef029c048cf977
Shubham Patra <babaijobmail1218@gmail.com>
2020-09-22 21:45:57 +0530

    Day 2 Complete Commit

--- src/com/Arvind/Company/Day2/Method1.java
+++ src/com/Arvind/Company/Day2/Method1.java
@@ -2,10 +2,23 @@ package com.Arvind.Company.Day2;
 
 public class Method1 {
 
+    //Access Modifier Object
     public void addNumber(int a,int b)
     {
         int sum = a+b;
-        System.out.println(sum);
+        System.out.println("Addition of "+a+" and "+b+" is: "+sum);
+    }
+    public void addNum()
+    {
+        int a = 10;
+        int b = 20;
+        int sum = a+b;
+        System.out.println("Addition of "+a+" and "+b+" is: "+sum);
+    }
+    public int addNumber1(int a, int b)
+    {
+        int sum = a+b;
+        return sum;
     }
     public static void main(String[] args) {
         /*int a = 10;
@@ -17,7 +30,13 @@ public class Method1 {
         int bb = 20;
         int sum1 = aa+bb;
         System.out.println(sum1);*/
-        //Class Reference   -  Class Object
+        //Class Reference =  Class Object
         Method1 me = new Method1();
+        me.addNumber(10,20);
+        me.addNumber(333,444);
+        me.addNumber(756,33);
+        me.addNum();
+        int x = me.addNumber1(856,457);
+        System.out.println(x);
     }
 }
