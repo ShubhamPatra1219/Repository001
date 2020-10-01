803de270adbe34fc5032d93ecdaae7ebef7833a4
Shubham Patra <babaijobmail1218@gmail.com>
2020-10-01 10:49:35 +0530

    My Second Commit

--- src/com/Arvind/Company/Day2/Method1.java
+++ src/com/Arvind/Company/Day2/Method1.java
@@ -7,6 +7,12 @@ public class Method1 {
         int sum = a+b;
         System.out.println(sum);
     }
+    public int addNumber1(int a,int b)
+    {
+        int sum = a+b;
+        System.out.println(sum);
+        return sum;
+    }
     public static void main(String[] args) {
         /*int a = 10;
         int b = 20;
@@ -19,5 +25,23 @@ public class Method1 {
         System.out.println(sum1);*/
         //Class Reference   -  Class Object
         Method1 me = new Method1();
+        me.addNumber(10,20);
+        me.addNumber(333,22);
+        me.addNumber(765,23);
+        int x = me.addNumber1(443,11);
+        System.out.println(x/2*6);
+        String val = me.studentName("Shubham");
+        System.out.println(val);
+        float f = me.per(4.444f);
+        System.out.println(f);
+    }
+    public String studentName(String name)
+    {
+        return name;
+    }
+
+    public float per(float val)
+    {
+        return val;
     }
 }
